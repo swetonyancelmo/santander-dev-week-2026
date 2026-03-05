3fa7d12a26e3de2922f7dce8c0a1b651a8c7d311
Swetony <swetonyancelmo@gmail.com>
2026-03-05 15:54:31 -0300

    Initial commit

---
+++ src/main/java/com/swetonyancelmo/SantanderDevWeek2026Application.java
@@ -0,0 +1,13 @@
+package com.swetonyancelmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeek2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/swetonyancelmo/SantanderDevWeek2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swetonyancelmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
